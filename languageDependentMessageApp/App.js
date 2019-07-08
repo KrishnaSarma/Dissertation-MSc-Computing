ddfbce8ac035366ae111a354c8bbc6bb0cd03da8
Krishna Sarma <krishna.sarma@smartpipesolutions.com>
2019-07-08 18:09:45 +0100

    after completion of the youtube tutorial and ethics form

--- languageDependentMessageApp/App.js
+++ languageDependentMessageApp/App.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, TextInput, Button, FlatList} from 'react-native';
+import axios from 'axios';
+import tail from 'lodash/tail';
+
+const serverUrl = 'http://192.168.0.13:5000';
+const http = axios.create({
+  baseURL: serverUrl,
+});
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -11,22 +18,81 @@ export default class App extends Component<Props> {
     };
   }
 
+  onLogin(){
+    const { isLoggedIn, username } = this.state;
+    if (!isLoggedIn){
+      http.post('/login', {username})
+        .then(() => this.onLoginSuccess())
+        .catch((err) => console.log(err))
+    }
+  }
+
+  onLoginSuccess(){
+    this.setState({isLoggedIn: true});
+    this.getMessages();
+  }
+
+  addMessage(data){
+    const { messages } = this.state;
+    const { id, message } = data;
+    messages.push(data)
+    this.setState({
+      lastUpdated: new Date(),
+      lastId: id,
+    });
+  }
+
+  addMessageList(msgList){
+    if (!msgList || msgList.length == 0){
+      return;
+    }
+    const { messages } = this.state;
+    this.setState ({
+      messages: [...messages, ...list],
+      lastUpdated: new Date(),
+      lastId: tail(msgList).id,
+    })
+
+  }
+
+  getMessages(){
+    const { lastId } = this.state;
+    http.get(lastId? '/get/${lastid}' : '/get')
+      .then((response) => this.addMessageList(response.data))
+      .catch((err) => console.log(err))
+  }
+
   onMessageSend(){
-    const {input, messages} = this.state;
-    messages.push(input);
+    const {input, username} = this.state;
+    http.post('/send', {
+      username,
+      message: input,
+    })
+      .then((response) => this.addMessage({
+        message: input,
+        id: response.data.id
+      }));
   }
 
   render() {
-    const { messages } = this.state;
+    const { messages, isLoggedIn, lastUpdated } = this.state;
     return (
       <View style={styles.container}>
+        <View>
+          <Text>Login</Text>
+          <TextInput
+             onChangeText = {(val) => this.setState({username: val})}
+           />
+           <Button title='Login' onPress={() => this.onLogin()} />
+           <Text>Online Status: {isLoggedIn ? 'Online':'Offline'}</Text>
+        </View>
         <FlatList
           data={messages}
-          renderItem = {({item}) => <Text>{item}</Text>}
+          renderItem = {({item}) => <Text>{item.message}</Text>}
+          extraData={this.state}
         />
         <TextInput
           onChangeText = {(val) => this.setState({input: val})}
-          style={styles.welcome}
         />
         <Button title="Send" onPress={() => this.onMessageSend()} />
       </View>
