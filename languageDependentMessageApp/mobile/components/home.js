7ee45502ad23f05ef85662a010f3b2391556e62a
Krishna Sarma <krishna.sarma@smartpipesolutions.com>
2019-08-17 00:52:51 +0100

    half way through welcome screen UI

--- languageDependentMessageApp/mobile/components/home.js
+++ languageDependentMessageApp/mobile/components/home.js
@@ -1,27 +1,119 @@
 import React, {Component} from 'react';
-import {Text, View, Button} from 'react-native';
-
+import {View, Text, ActivityIndicator, Button, ImageBackground} from 'react-native';
 export default class HomeScreen extends Component{
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            loading: true
+        };
+    }
+
+    componentDidMount(){
+        this.setState({
+            loading: false
+        })
+    }
 
     
     render(){
         const {navigate} = this.props.navigation;
-        return(
-            <View>
-                <Text style = {{height: 40, width: 400, textAlign: "center" }}>
-                    Welcome to MyChatApp
-                </Text>
-                <Text style = {{height: 40, width: 400, textAlign: "center"}}>
-                    Click the Signin button to Sign In
-                </Text>
-                <Button 
-                    style={{ height : 40, width : 40 }}
-                    onPress={() => navigate('Login')}
-                    title="Sign In"
-                />
-                <Text style = {{height : 40, width : 400, textAlign: "center"}} onPress = {() => navigate('Signup')}>
-                    New to the app? Signup here!
-                </Text>
+        return(        
+            <View
+            style = {{
+                // flex: 1,
+                // flexDirection: "column",
+                justifyContent: 'center',
+                alignSelf: "center",
+                backgroundColor: "red"
+                // width: "100%"
+            }}>
+                {
+                    this.state.loading ? (
+                        <View><ActivityIndicator size="large" color="grey" /></View>
+                    ) : (
+                        <View 
+                        style = {{height: "100%",
+                        width: "100%",
+                        flex: 1,
+                        justifyContent: "center",
+                        alignSelf: "center"
+                        }}>                       
+                            <ImageBackground source={require('../images/welcome_background1.jpg')} style={{
+                            // flex: 1,
+                            resizeMode: 'cover',
+                            justifyContent: "center",
+                            width: "100%",
+                            height: "100%"
+                            // opacity: 0.5
+                            }}>
+                                <View 
+                                style = {{
+                                justifyContent: "space-around",
+                                alignSelf: "center",
+                                backgroundColor: "transparent",
+                                height: "80%",
+                                width: "90%"
+                                }}>
+                                    <View style={
+                                        {flex: 1,
+                                        backgroundColor: "pink",
+                                    justifyContent: "center"}
+                                    }>
+
+                                    <Text style={{
+                                        // margin: 20,
+                                                fontSize: 45,
+                                                fontWeight: "bold",
+                                                textAlign: "center"
+                                            }}>
+                                        Welcome to
+                                    </Text>
+                                    <Text style = {{
+                                        // margin: 20,
+                                                fontSize: 40,
+                                                fontWeight: "bold",
+                                                textAlign: "center",
+                                                color: "red"
+                                            }}>
+                                        myChat
+                                    </Text>
+                                    </View>
+                                    <View style= {{flex: 1, backgroundColor: "green", justifyContent: "center"}}>
+
+                                    <Button
+                                        style={{ 
+                                            // justifyContent: "center",
+                                            alignSelf: "center", 
+                                            textAlign: "center",
+                                            fontSize: 20}}
+                                        onPress={() => navigate('Login')}
+                                        title="LOG IN">
+                                    </Button>
+                                    <Button
+                                        style={{ 
+                                            // justifyContent: "center",
+                                            alignSelf: "center", 
+                                            textAlign: "center",
+                                            fontSize: 20}}
+                                        onPress={() => navigate('Signup')}
+                                        title="SIGN UP">
+                                    </Button>
+                                    {/* <Button 
+                                        style={{ height : 40, width : 40 }}
+                                        onPress={() => navigate('Login')}
+                                        title="Sign In"
+                                    /> */}
+                                    {/* <Text style = {{height : 40, width : 500, textAlign: "center"}} onPress = {() => navigate('Signup')}>
+                                        New to the app? Signup here!
+                                    </Text> */}
+                                    </View>
+                                </View>
+                            </ImageBackground>
+                        </View>
+                    )
+                }
             </View>
         )
     }
